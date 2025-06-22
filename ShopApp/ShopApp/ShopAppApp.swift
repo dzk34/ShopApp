6f3e1b9379bcce03e73fad00bdbacc726b6924c8
dzk34 <khaledus34@gmail.com>
2025-06-22 17:14:20 +0100

    xcode project setup

---
+++ ShopApp/ShopApp/ShopAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShopAppApp.swift
+//  ShopApp
+//
+//  Created by Khaled on 22/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct ShopAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ShopApp/ShopAppTests/ShopAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  ShopAppTests.swift
+//  ShopAppTests
+//
+//  Created by Khaled on 22/06/2025.
+//
+
+import Testing
+@testable import ShopApp
+
+struct ShopAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
