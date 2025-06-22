d599790cd95659feb9d4e4600b84a721c26a8eda
dzk34 <khaledus34@gmail.com>
2025-06-22 17:18:48 +0100

    Added a skeletong for a Coordinator

---
+++ ShopApp/ShopApp/Coordinator.swift
@@ -0,0 +1,30 @@
+//
+//  Coordinator.swift
+//  ShopApp
+//
+//  Created by Khaled on 22/06/2025.
+//
+
+import Foundation
+
+protocol CoordinatorProtocol {
+    func push(page: String)
+    func pop()
+    func popToRoot()
+}
+
+
+class Coordinator: CoordinatorProtocol {
+    func push(page: String) {
+        
+    }
+    
+    func pop() {
+        
+    }
+    
+    func popToRoot() {
+        
+    }
+    
+}
