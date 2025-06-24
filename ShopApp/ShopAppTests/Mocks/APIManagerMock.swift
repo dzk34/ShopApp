f54adba3cbcba707a5ffa991d52c5717a302ea94
dzk34 <khaledus34@gmail.com>
2025-06-24 09:51:10 +0100

    Added mocks for unit tests

---
+++ ShopApp/ShopAppTests/Mocks/APIManagerMock.swift
@@ -0,0 +1,16 @@
+//
+//  APIManagerMock.swift
+//  ShopAppTests
+//
+//  Created by Khaled on 24/06/2025.
+//
+
+import Foundation
+
+@testable import ShopApp
+
+struct APIManagerMock: APIManagerProtocol {
+    func perform(_ request: RequestProtocol) async throws -> Data {
+        return try Data(contentsOf: URL(fileURLWithPath: request.path), options: .mappedIfSafe)
+    }
+}
---
+++ ShopApp/ShopAppTests/Mocks/ProductsRequestMock.swift
@@ -0,0 +1,23 @@
+//
+//  ProductsRequestMock.swift
+//  ShopAppTests
+//
+//  Created by Khaled on 24/06/2025.
+//
+
+import Foundation
+
+@testable import ShopApp
+
+enum ProductsRequestMock: RequestProtocol {
+    case getProducts
+
+    var requestType: RequestType {
+        return .get
+    }
+
+    var path: String {
+        guard let path = Bundle.main.path(forResource: "Product", ofType: "json") else { return "" }
+        return path
+    }
+}
